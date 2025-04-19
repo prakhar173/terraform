cb0a462c7a6506bb31f5aea057a1b061fa6012e0
Prakhar Mishra <72335934+prakhar173@users.noreply.github.com>
2025-04-20 00:38:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,8 +35,6 @@ provider "kubernetes" {
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 
-  # Ensure this waits until the cluster is created
-  depends_on = [google_container_cluster.primary]
 }
 
 resource "kubernetes_namespace" "example" {

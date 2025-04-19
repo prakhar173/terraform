89c9f792f333bfa3551df618a0d5bbaa3daa1dbb
Prakhar Mishra <72335934+prakhar173@users.noreply.github.com>
2025-04-19 20:10:08 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${google_container_cluster.primary.name}-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = var.region
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+  }
+}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
+
+data "google_client_config" "default" {}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = var.namespace
+  }
+}

ca38dbae6664dec2e0bb159b19da3e3b32c20827
Prakhar Mishra <72335934+prakhar173@users.noreply.github.com>
2025-04-20 12:23:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,7 @@ resource "google_container_node_pool" "primary_nodes" {
   cluster    = google_container_cluster.primary.name
   location   = var.region
   node_count = 1
+  disk_type = "pd-standard"  # default is usually "pd-ssd"
 
   node_config {
     machine_type = "e2-medium"

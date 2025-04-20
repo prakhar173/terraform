5d0cde8ce90b5258e59a05dcc78d4e9f8bff3c44
Prakhar Mishra <72335934+prakhar173@users.noreply.github.com>
2025-04-20 13:36:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,9 +12,19 @@ resource "google_compute_network" "vpc" {
 resource "google_compute_subnetwork" "subnet" {
   name          = "gke-subnet"
   ip_cidr_range = "10.0.0.0/16"
-  region        = "us-central1"
+  region        = var.region
   network       = google_compute_network.vpc.id
-} 
+
+  secondary_ip_range {
+    range_name    = "pods"
+    ip_cidr_range = "10.1.0.0/16"
+  }
+
+  secondary_ip_range {
+    range_name    = "services"
+    ip_cidr_range = "10.2.0.0/20"
+  }
+}
 
 resource "google_container_cluster" "primary" {
   name     = "prod-cluster"
